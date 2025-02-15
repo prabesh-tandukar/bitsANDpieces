d2fd775ba56b5d3cf590869c680587be814c4da7
Prabesh <tandukarprabesh@gmail.com>
2025-02-16 08:40:51 +1300

    exe 3

---
+++ EloquentJavascript/Chap5/exe3.js
@@ -0,0 +1,31 @@
+// Everything
+// Arrays also have an every method analogous to the some method. This method returns true when the given function returns true for every element in the array. In a way, some is a version of the || operator that acts on arrays, and every is like the && operator.
+
+// Implement every as a function that takes an array and a predicate function as parameters. Write two versions, one using a loop and one using the some method.
+
+// function every(array, test) {
+//   // Your code here.
+// }
+
+// console.log(every([1, 3, 5], n => n < 10));
+// // → true
+// console.log(every([2, 4, 16], n => n < 10));
+// // → false
+// console.log(every([], n => n < 10));
+// // → true
+
+function every(array, test) {
+  let final = true;
+  for (let element of array) {
+    if (!test(element)) {
+      final = false;
+    } else {
+      final = true;
+    }
+  }
+  return final;
+}
+
+console.log(every([1, 3, 5], (n) => n < 10));
+console.log(every([2, 4, 16], (n) => n < 10));
+console.log(every([], (n) => n < 10));
